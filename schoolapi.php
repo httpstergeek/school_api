59d0ea8a707e162d756631278ea7d1fb80ec3a18
Bernardo Macias <bmacias@httpstergeek.com>
2015-02-21 14:41:55 -0800

    Update php for json

--- schoolapi.php
+++ schoolapi.php
@@ -5,6 +5,8 @@ $schools = file_get_contents("data/seattle_schools.json");
 $json_encoded = json_decode($schools, true);
 // checking for filter arguments
 if (!empty($_REQUEST)) {
+    // decoding local file
+    $json_encoded = json_decode($schools, true);
     // creating array will uses as associative
     $matches = array();
     // foreach element in json object
@@ -24,9 +26,18 @@ if (!empty($_REQUEST)) {
     }
     // adds key to top level.
     $schools = array("schools" => $matches);
+    $schools = json_encode($schools, JSON_PRETTY_PRINT);
 } else {
     $schools = $json_encoded;
 }
 // encodes object as json with pretty print
-header('Content-type: application/javascript');
-echo json_encode($schools);
+if (array_key_exists('callback', $_GET)) {
+    header('Content-Type: text/javascript; charset=utf8');
+    $callback = $_GET['callback'];
+    echo $callback . '(' . $schools . ');';
+} else {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET');
+    header('Content-Type: application/json; charset=utf8');
+    echo $schools;
+}
